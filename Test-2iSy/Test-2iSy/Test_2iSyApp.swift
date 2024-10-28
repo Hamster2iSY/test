a6496e0550d5aed30602d9b79fa00d8048c11c7c
Yasser Farahi <github@risinggalaxy.com>
2024-10-28 13:13:04 +0100

    project initial commit

---
+++ Test-2iSy/Test-2iSy/Test_2iSyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_2iSyApp.swift
+//  Test-2iSy
+//
+//  Created by Yasser Farahi on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_2iSyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test-2iSy/Test-2iSyTests/Test_2iSyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_2iSyTests.swift
+//  Test-2iSyTests
+//
+//  Created by Yasser Farahi on 28/10/2024.
+//
+
+import Testing
+@testable import Test_2iSy
+
+struct Test_2iSyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
